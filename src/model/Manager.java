ed5921d57fa19cd1da97621769f7dbc6dc8cfff9
Gonzalo De Varona Osorio <gonzalodevaronaosorio@MacBook-Pro-de-Gonzalo.local>
2019-10-01 10:11:38 -0500

    First commit

---
+++ src/model/Manager.java
@@ -0,0 +1,17 @@
+/*
+* ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+* ID
+* UNIVERSIDAD ICESI (CALI-COLOMBIA)
+* DEPARTAMENTO TIC - ALGORTIMOS Y PROGRAMACIÓN II
+* LAB IV
+* @AUTHOR: GONZALO DE VARONA <gonzalo.de1@correo.icesi.edu.co>
+* @LAST UPDATE DATE: 30 SEPTEMBER 2019
+* ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+*/
+
+
+package model;
+
+public class Manager {
+
+} //end of class
---
+++ src/model/Player.java
@@ -0,0 +1,88 @@
+/*
+* ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+* ID
+* UNIVERSIDAD ICESI (CALI-COLOMBIA)
+* DEPARTAMENTO TIC - ALGORTIMOS Y PROGRAMACIÓN II
+* LAB IV
+* @AUTHOR: GONZALO DE VARONA <gonzalo.de1@correo.icesi.edu.co>
+* @LAST UPDATE DATE: 30 SEPTEMBER 2019
+* ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+*/
+
+package model;
+
+import java.util.GregorianCalendar;
+
+public class Player {
+	String id;
+	String firstName;
+	String lastName;
+	String email;
+	char gender;
+	
+	GregorianCalendar birthDate;
+
+	public Player(String id, String firstName, String lastName, String email, char gender,
+			GregorianCalendar birthDate) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.gender = gender;
+		this.birthDate = birthDate;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public GregorianCalendar getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(GregorianCalendar birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	
+	
+	
+
+} //end of class
